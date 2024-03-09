cf68eaa7a7eea65492908e5551b40359d4088630
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2024-03-09 15:36:47 +0300

    Анимация для персонажа

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float lerpTime;
 
     private Rigidbody2D rigidBody;
+    private Animator _animator;
     private Vector2 inputVector;
     private Vector2 lerpInputVector;
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,6 +22,10 @@ public class PlayerMovement : MonoBehaviour
         inputVector.Normalize();
         lerpInputVector = Vector2.Lerp(lerpInputVector, inputVector, Time.deltaTime * lerpTime);
         rigidBody.velocity = lerpInputVector * playerSpeed;
+
+        _animator.SetFloat("Horizontal", inputVector.x);
+        _animator.SetFloat("Vertical", inputVector.y);
+        _animator.SetFloat("Speed", inputVector.magnitude);
     }
 
     public void GetInputVector(InputAction.CallbackContext context)
