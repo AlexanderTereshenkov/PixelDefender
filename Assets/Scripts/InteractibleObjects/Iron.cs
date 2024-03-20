028ce47aa3bff1a86ba2353a3b1d7bca399ae27a
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2024-03-20 23:50:20 +0300

    Абстрактный класс для предметов, которые можно сломать

--- Assets/Scripts/InteractibleObjects/Iron.cs
+++ Assets/Scripts/InteractibleObjects/Iron.cs
@@ -1,31 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Iron : MonoBehaviour, IInteractible
+public class Iron : BreakableObject, IInteractible
 {
-    private int ironHealth = 10;
-    private bool isPossibleToGet = false;
     public void Action(Inventory inventory, string tool)
     {
-        if (isPossibleToGet && tool == "Pickaxe")
+        if (isPossibleToGet && tool.Equals(usableTool))
         {   
-            ironHealth -= Random.Range(1, 4);
-            if (ironHealth <= 0)
+            health -= Random.Range(1, 4);
+            if (health <= 0)
             {
                 inventory.Iron += 1;
                 Destroy(gameObject);
             }
         }
-    }
-
-    private void OnMouseEnter()
-    {
-        isPossibleToGet = true;
-    }
-
-    private void OnMouseExit()
-    {
-        isPossibleToGet = false;
     }
 }
--- Assets/Scripts/InteractibleObjects/Stone.cs
+++ Assets/Scripts/InteractibleObjects/Stone.cs
@@ -1,31 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Stone : MonoBehaviour, IInteractible
+public class Stone : BreakableObject, IInteractible
 {
-    private int stoneHealth = 7;
-    private bool isPossibleToGet = false;
     public void Action(Inventory inventory, string tool)
     {
-        if (isPossibleToGet && tool == "Pickaxe")
+        if (isPossibleToGet && tool.Equals(usableTool))
         {
-            stoneHealth-=1;
-            if(stoneHealth <= 0)
+            health-=1;
+            if(health <= 0)
             {
                 inventory.Stone += 1;
                 Destroy(gameObject);
             }
         }
-    }
-
-    private void OnMouseEnter()
-    {
-        isPossibleToGet = true;
-    }
-
-    private void OnMouseExit()
-    {
-        isPossibleToGet = false;
     }
 }
