a897a7f01f4bdb99deae39b34f63160af165f99c
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2024-04-04 23:51:15 +0300

    Смена дня и ночи, UI часов

---
+++ Assets/Scripts/Gameplay/Timer/ClockUI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ClockUI : MonoBehaviour
+{
+    [SerializeField]private WorldTime worldTime;
+    private PlayerUI playerUI;
+
+    private void Start()
+    {
+        playerUI = SingleGameEnterPoint.instance.GetPlayerUI();
+    }
+
+    private void OnEnable()
+    {
+        worldTime.OnTimeChaged += UpdateTimerUI;
+    }
+
+    private void OnDisable()
+    {
+        worldTime.OnTimeChaged -= UpdateTimerUI;
+    }
+
+    private void UpdateTimerUI()
+    {
+        playerUI.Timertext = worldTime.GetTime();
+    }
+}
