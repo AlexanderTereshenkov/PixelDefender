e5f67709b7017b01de82b15fd8205cfa1394a9d1
Gleb <koanec5000@gmail.com>
2024-04-21 17:49:52 +0300

    Создан спавнер для мобов

---
+++ Assets/Scripts/Gameplay/MobAttack/MobSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpapwner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform spawnPoint;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
