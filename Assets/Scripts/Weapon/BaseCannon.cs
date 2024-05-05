2efd0a427629dbef8e44946641b3511824d7d973
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2024-05-06 00:43:20 +0300

    Небольшие дополнения в спавнер пушек

--- Assets/Scripts/Weapon/BaseCannon.cs
+++ Assets/Scripts/Weapon/BaseCannon.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseCannon : MonoBehaviour, IInteractible
 {
-    private GameObject cannon;
 
+    [SerializeField] private int canonPrice;
+
+    private GameObject cannon;
 
     public void Action(Inventory inventory)
     {
@@ -17,9 +17,9 @@ public class BaseCannon : MonoBehaviour, IInteractible
 
     private void PutCannon(Inventory inventory)
     {
-        if(inventory.IronIngot >= 5)
+        if(inventory.IronIngot >= canonPrice)
         {
-            inventory.IronIngot -= 5;
+            inventory.IronIngot -= canonPrice;
             GameObject cannonToCreate = CannonManager.main.GetCannon();
             cannon = Instantiate(cannonToCreate, transform.position, Quaternion.identity);
         }
