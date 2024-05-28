6d6b4500aaf4b5405673903907c4c6df02cf17f8
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2024-05-29 01:41:30 +0300

    Исправление багов, добавили пасхалку (секрет куда)

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,16 +1,36 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     [SerializeField] private Texture2D defaultCursor;
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Sprite monkeyPicture;
+    [SerializeField] private Sprite standartPicture;
+
+    private bool isFunnyPictureShown = false;
 
     private void Start()
     {
         Time.timeScale = 1;
         Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
+
+    public void ShowCoolPicture()
+    {
+        if (!isFunnyPictureShown)
+        {
+            backgroundImage.sprite = monkeyPicture;
+            isFunnyPictureShown = true;
+            return;
+        }
+        backgroundImage.sprite = standartPicture;
+        isFunnyPictureShown = false;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
